97f940ba97e61e0b6d49cb8f2852d244b56f9ff8
Amdrii <andreyk557@gmail.com>
2023-02-02 15:35:01 +0200

    feat[color-change]

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const board = document.querySelector('#board')
-const SQuares_number = 450
+const SQuares_number = 500
+
+const colors = ['red', 'blue', 'green', 'yellow', 'purple']
 
 for (let i = 0; i < SQuares_number; i++){
     const square =document.createElement('div')
     square.classList.add('square')
 
     square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseleave', () => removeColor(square))
 
     board.append(square)
 }
 
 function setColor(element) {
-    element.style.backgroundColor = 'red'
+    const color =getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length)
+  return colors[index]
 }
